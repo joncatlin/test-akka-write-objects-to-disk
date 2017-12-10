151a808a7535deae780aed13eb4f4aabeee8555b
DESKTOP-CM4DN2T\joncatlin <jonc@destini.com>
2017-12-10 08:26:06 -0700

    Adding the missing files to the repo.

---
+++ SnapShotStore/FileSnapshotStore.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using System.IO;
+using System.Threading;
+using System.Runtime.Serialization;
+using Akka.Routing;
+using Akka.Persistence.Snapshot;
+using Akka.Persistence;
+using System.Threading.Tasks;
+using Akka.Event;
+using Akka.Dispatch;
+
+namespace SnapShotStore
+{
+
+    class FileSnapshotStore : SnapshotStore
+    {
+        private ILoggingAdapter _log;
+        private readonly int _maxLoadAttempts;
+        private readonly MessageDispatcher _streamDispatcher;
+        private readonly DirectoryInfo _dir;
+        private readonly ISet<SnapshotMetadata> _saving;
+        private readonly Akka.Serialization.Serialization _serialization;
+        private string _defaultSerializer;
+
+        private const int NUM_ACTORS = 4;
+        private static int NumActors = NUM_ACTORS;
+
+        private static int GetActorNumber()
+        {
+            if (NumActors < 0)
+            {
+                // Throw an exception
+            }
+            return NumActors--;
+        }
+
+        private readonly int ActorNumber;
+
+        public FileSnapshotStore()
+        {
+            // Get this actors number in the pool
+            ActorNumber = FileSnapshotStore.GetActorNumber();
+
+            // Get the configuration
+            var config = Context.System.Settings.Config.GetConfig("akka.persistence.snapshot-store.jonfile");
+            _maxLoadAttempts = config.GetInt("max-load-attempts");
+
+//            _streamDispatcher = Context.System.Dispatchers.Lookup(config.GetString("stream-dispatcher"));
+            _dir = new DirectoryInfo(config.GetString("dir"));
+
+//            _defaultSerializer = config.GetString("serializer");
+
+            _serialization = Context.System.Serialization;
+            _saving = new SortedSet<SnapshotMetadata>(SnapshotMetadata.Comparer); // saving in progress
+            _log = Context.GetLogger();
+        }
+
+        protected override Task DeleteAsync(SnapshotMetadata metadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task DeleteAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<SelectedSnapshot> LoadAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task SaveAsync(SnapshotMetadata metadata, object snapshot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ SnapShotStore/TestActor.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka;
+using Akka.Actor;
+using Akka.Persistence;
+
+
+namespace SnapShotStore
+{
+    #region Command and Message classes
+    // The message to be published
+    public class SomeMessage
+    {
+        public SomeMessage(Account acc)
+        {
+            this.Acc = acc;
+        }
+
+        public Account Acc { get; private set; }
+    }
+    #endregion
+
+    class TestActor : ReceivePersistentActor
+    {
+
+        // The actor state to be persisted
+        private readonly Account Acc;
+
+        public override string PersistenceId
+        {
+            get
+            {
+                return Acc.AccountID;
+            }
+        }
+
+        public TestActor(Account acc)
+        {
+            // Store the actor state 
+            Acc = acc;
+
+            // Recover
+            Recover<SnapshotOffer>(offer => RecoverSnapshot(offer));
+
+            // Commands
+            Command<SaveSnapshotSuccess>(cmd => SnapshotSuccess(cmd));
+            Command<SaveSnapshotFailure>(cmd => SnapshotFailure(cmd));
+            Command<SomeMessage>(msg => Process(msg));
+        }
+
+
+
+        private void SnapshotSuccess(SaveSnapshotSuccess cmd)
+        {
+            Console.WriteLine("Processing SnapShotSuccess command");
+
+        }
+
+        private void SnapshotFailure(SaveSnapshotFailure cmd)
+        {
+            Console.WriteLine("Processing SnapShotFailure command");
+
+        }
+
+        private void Process(SomeMessage msg)
+        {
+            SaveSnapshot(Acc);
+
+            // Show the system configuration
+            Console.WriteLine(Context.System.Settings.Config);
+
+        }
+
+        private void RecoverSnapshot(SnapshotOffer offer)
+        {
+            Console.WriteLine("Processing RecoverSnapshot");
+
+        }
+
+
+
+
+    }
+}
